6e0da6670a4d0712c5d8909126b327900d966936
Shivasurya7 <128982338+Shivasurya7@users.noreply.github.com>
2023-06-18 18:53:12 +0530

    coputer

--- Print palindrome numbers from array.cpp
+++ Print palindrome numbers from array.cpp
@@ -24,31 +24,3 @@ int main()
 		}
   }
 }
-
-/*Solution 2:(using recursion)
-
-#include<iostream>  
-using namespace std; 
-void fib(int n,int option,int n1 = 0,int n2 = 1){
-  int n3 = n1+n2;
-  if(n == option){ //to stop at the right number
-     if(n1%2 != option%2){ // to print even for option 1 and odd for option 2
-        cout<<n1<<" ";
-      }
-    return; 
-  }
-  fib(n-1,option,n2,n3);//swaping the value to find next fib number
-  if(n1%2 != option%2){
-      cout<<n1<<" ";
-   }
-}
-int main()  
-{
-  int n,option;
-  cout<<"enter the nuber :";
-  cin>>n;
-  cout<<"enter the option : ";
-  cin>>option;
-  fib(n+2,option); // adding to for to print the requered values has to come in n1 to print starting values 
-} 
-*/
